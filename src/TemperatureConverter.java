c7880f65fb61c23f79b43f00ed1b87e083c33fc8
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2018-12-27 16:01:58 -0500

    Update TemperatureConverter.java

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -1,28 +1,3 @@
-/*
- * TemperatureConverter.java
- * Author: Cynthia He
- * Submission Date:
- *
- * Purpose: The program does the following: prompts the user to enter a temperature in Fahrenheit
- * and a temperature scale the user wants to convert it to. If both the temperature in Fahrenheit to
- * the specified temperature scale and displays the converted value.
- *
- * Statement of Academic Honesty:
- *
- * The following code represents my own work. I have neither
- * received nor given inappropriate assistance. I have not copied
- * or modified code from any source other than the course webpage
- * or the course textbook. I recognize that any unauthorized
- * assistance or plagiarism will be handled in accordance with
- * the University of Georgia's Academic Honesty Policy and the
- * policies of this course. I recognize that my work is based
- * on an assignment created by the Department of Computer Science
- * at the University of Georgia. Any publishing
- * or posting of source code for this project is strictly
- * prohibited unless you have written consent from the Department
- * of Computer Science at the University of Georgia.
- */
-
 import java.util.Scanner;
 
 public class TemperatureConverter{
